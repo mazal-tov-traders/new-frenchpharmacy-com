80046d585a68f19b20989b9240356925970f8ec0
mazal-tov-traders <d90.german@gmail.com>
2024-09-25 11:31:01 +0300

    start create script displaying the price in the buy button

--- assets/global.js
+++ assets/global.js
@@ -1408,3 +1408,5 @@ class AccountIcon extends HTMLElement {
 }
 
 customElements.define('account-icon', AccountIcon);
+
+const links = document.links; for (let i = 0, linksLength = links.length ; i < linksLength ; i++) { if (links[i].hostname !== window.location.hostname) { links[i].target = '_blank'; links[i].rel = 'noreferrer noopener'; } }
